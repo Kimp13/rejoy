2370c4b4a57b0ee8976c520ba89c680ebfc50abf
Kimp13 <akunec41@gmail.com>
2020-07-18 19:36:12 +0300

    Версия 0.1.0 с центральным блоком
    Думаю, стоить коммитить на русском. Что же добавилось? Иконки прямо с сайта fontawesome.com стали качественнее, шапка теперь убирается, появились центральная и нижняя панели.

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,175 +1,188 @@
 <template>
   <Page actionBarHidden="true">
-    <GridLayout class="wrapper" columns="*" rows="auto, *, 50">
-      <StackLayout class="main-control-panel" row="0" orientation="vertical">
-        <Image width="20" height="20" horizontalAlignment="right" src="~/assets/icons/home.png" v-on:tap="returnHome" />
-        <AbsoluteLayout>
-          <TextField left="0" top="0" height="40" />
-          <Image left="10" top="35" width="20" height="20" src="~/assets/icons/search.png"/>
-        </AbsoluteLayout>
-        <ScrollView orientation="horizontal" scrollBarIndicatorVisible="false" class="main-control-panel-buttons">
-          <FlexboxLayout justifyContent="space-between">
-            <Button width="55" height="55" text="" @tap="event" />
-            <Button width="55" height="55" text="" @tap="event" />
-            <Button width="55" height="55" text="" @tap="event" />
-            <Button width="55" height="55" text="" @tap="event" />
-            <Button width="55" height="55" text="" @tap="event" />
-            <Button width="55" height="55" text="" @tap="event" />
-            <Button width="55" height="55" text="" @tap="event" />
-            <Button width="55" height="55" text="" @tap="event" />
-          </FlexboxLayout>
-        </ScrollView>
-      </StackLayout>
-      <ScrollView orientation="vertical" row="1" @scroll="scroll">
-        <StackLayout orientation="vertical">
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
-          <Label>lorem</Label>
+    <GridLayout class="wrapper" columns="*" :rows="gridRows">
+      <ScrollView class="main" orientation="vertical" row="0" rowSpan="2" col="0">
+        <StackLayout class="main-container" orientation="vertical">
+          <template v-for="(category, index) in serviceCategories">
+            <Label class="main-container-label" :text="category.name" :key="'l' + index"/>
+            <ScrollView class="main-container-scroll" :key="'s' + index" orientation="horizontal" scrollBarIndicatorVisible="false">
+                <FlexboxLayout justifyContent="center">
+                  <AbsoluteLayout v-for="(item, index) in category.services" :key="'a' + index" class="main-container-scroll-item" :style="{backgroundImage: `url('${item.image}')`}" />
+                </FlexboxLayout>
+            </ScrollView>
+          </template>
         </StackLayout>
       </ScrollView>
-      <GridLayout class="footer" row="2" col="0" columns="*, *, *, *, *" rows="*">
-        <Label col="0">
-          <Button text="test"/>
-        </Label>
-        <Label col="1"> 
-          <Button text="test"/>
-        </Label>
-        <Label col="2">
-          <Button text="test"/>
-        </Label>
-        <Label col="3">
-          <Button text="test"/>
-        </Label>
-        <Label col="4">
-          <Button text="test"/>
-        </Label>
-      </GridLayout>
+      <AbsoluteLayout class="header" row="0" col="0">
+        <Image class="header-background" top="0" left="0" src="~/assets/images/header0.png" />
+        <DockLayout class="header-nav" top="0" left="0" stretchLastChild="false">
+          <Image v-if="headerOpened" class="header-nav-toggle" width="30" height="30" dock="left" @tap="toggleHeader" src="~/assets/icons/times-solid.png" />
+          <Image v-else class="header-nav-toggle" width="30" height="30" dock="left" @tap="toggleHeader" src="~/assets/icons/bars-solid.png" />
+          <Image class="header-nav-home" width="30" height="30" dock="right" src="~/assets/icons/home-solid.png" @tap="returnHome" />
+        </DockLayout>
+        <AbsoluteLayout class="header-search" left="0" top="65">
+          <TextField class="header-search-field" left="0" top="0" />
+          <Image left="20" top="12.5" width="20" height="20" scaleX="-1" src="~/assets/icons/search-solid.png" />
+        </AbsoluteLayout>
+        <ScrollView top="137" orientation="horizontal" scrollBarIndicatorVisible="false" class="header-buttons">
+          <FlexboxLayout justifyContent="space-between">
+            <Button text=""  />
+            <Button text=""  />
+            <Button text=""  />
+            <Button text=""  />
+            <Button text=""  />
+            <Button text=""  />
+            <Button text=""  />
+            <Button text=""  />
+          </FlexboxLayout>
+        </ScrollView>
+      </AbsoluteLayout>
+      <StackLayout class="footer" orientation="horizontal" row="2" col="0">
+        <Label text="1" />
+        <Label text="2" />
+        <Label text="3" />
+        <Label text="4" />
+        <Label text="5" />
+      </StackLayout>
     </GridLayout>
   </Page>
 </template>
 
 <script>
+  function returnHome() {
+    console.log(`Grid first row spans ${this.gridFirstRow} pixels.`);
+  };
+
+  function toggleHeader(e) {
+    if (this.headerOpened) {
+      this.headerOpened = false;
+      this.gridFirstRow = '60';
+    } else {
+      this.headerOpened = true;
+      this.gridFirstRow = '225';
+    }
+  };
+
+  let serviceCategories = [
+    {
+      name: 'Выгул собак',
+      services: [
+        {
+          image: '~/assets/images/dogs0.png',
+        },
+        {
+          image: '~/assets/images/dogs1.png',
+        }
+      ]
+    },
+    {
+      name: 'Выгул котов',
+      services: [
+        {
+          image: '~/assets/images/cats0.png',
+        },
+        {
+          image: '~/assets/images/cats1.png',
+        }
+      ]
+    }
+  ];
+
   export default {
     data() {
       return {
-        previousScrolls: [
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-        ],
-        sumOfPreviousScrolls: 0,
-        previousScroll: 0
+        headerOpened: false,
+        gridFirstRow: '60',
+        serviceCategories
       }
     },
     methods: {
-      returnHome: e => {
-        console.log('Returning home on', e.eventName);
-      },
-      event: e => {
-        console.log(e.eventName);
-      },
-      scroll: function(event) {
-        let difference = (this.previousScroll - event.scrollY),
-            newPreviousScrolls = [];
-
-        for (let i = 1; i < 10; i += 1) {
-          newPreviousScrolls[i-1] = this.previousScrolls[i];
-        }
-
-        newPreviousScrolls[9] = difference;
-
-        this.sumOfPreviousScrolls += (difference - this.previousScrolls[0]);
-        this.previousScrolls = newPreviousScrolls;
-
-        this.previousScroll = event.scrollY;
+      toggleHeader,
+      returnHome
+    },
+    computed: {
+      gridRows: function() {
+        return this.gridFirstRow + ', *, 50';
       }
     }
   }
 </script>  
 
 <style lang="scss">
-* {
-  z-index: 0;
-}
-.wrapper {
-  .header {
-    button {
-      background: transparent;
-      border-radius: 50%;
-      z-index: 1;
+.header {
+  width: 100%;
+  * {
+    z-index: 1;
+  }
+  &-background {
+    width: 100%;
+    z-index: 0;
+  }
+  &-nav {
+    width: 100%;
+    height: 60;
+    padding: 0 10;
+    image {
+      opacity: .6;
     }
   }
-  .main {
-    &-control-panel {
-      background-image: url('~/assets/images/control-panel.png');
-      background-size: cover;
-      background-repeat: no-repeat;
-      background-position: center;
-      padding: 20 20 40;
-      textfield {
-        width: 100%;
-        margin: 26 0 32;
-        border-color: white;
-        background-color: white;
-        opacity: .4;
+  &-search {
+    width: 100%;
+    &-field {
+      width: 95%;
+      margin: 0 2.5%;
+      border-color: white;
+      background-color: white;
+      opacity: .6;
+      border-radius: 10;
+      padding: 10 10 10 40;
+    }
+    image {
+      opacity: .6;
+    }
+  }
+  &-buttons {
+    width: 100%;
+    button {
+      width: 55;
+      height: 55;
+      background-color: white;
+      opacity: .4;
+      border-radius: 50%;
+      margin: 0 15;
+    }
+  } 
+}
+.main {
+  margin: 60 0 0 0;
+  &-container {
+    &-label {
+      font-style: italic;
+      font-size: 25;
+      margin: 10 25;
+    }
+    &-scroll {
+      &-item {
+        width: 250;
+        height: 120;
         border-radius: 10;
-        padding: 10 10 10 40;
-      }
-      &-buttons {
-        width: 100%;
-        button {
-          background-color: white;
-          opacity: .4;
-          border-radius: 50%;
-          margin: 0 15;
-        }
+        margin: 0 10;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
       }
     }
+  }
+}
+
+.footer {
+  width: 100%;
+  label {
+    width: 20%;
+    background: #f1f1f1;
+    font-size: 30;
+    font-weight: 900;
+    text-align: center;
   }
 }
 </style>
